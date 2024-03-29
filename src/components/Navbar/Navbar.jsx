415987dc28d45f21fed521c70698c3dba7863ac8
QuentinGM <quentin.girerd-martin@laposte.net>
2024-03-29 15:42:36 +0100

    Dernière version à 15h42

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -7,7 +7,7 @@ function Navbar() {
     <nav className="navbar">
       <NavLink to="/" exact="" activeclassname="active"> {/* Passer exact en tant que chaîne de caractères */}
         <div className="navbar__logo">
-          <img src="logo.png" alt="Logo" />
+          <img src="/logo.png" alt="Logo" />
         </div>
       </NavLink>
       <NavLink to="/" exact="" activeclassname="active"> {/* Passer exact en tant que chaîne de caractères */}
--- src/layout/Footer/Footer.jsx
+++ src/layout/Footer/Footer.jsx
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <div className="footer">
         <div className="footer__logo">
-            <img src="logo2.png" alt="logo" width="100" />
+            <img src="/logo2.png" alt="logo" width="100" />
         </div>
         <div className="footer__text"> © 2020 Kasa. All rights reserved</div>
     </div>
